750a99c3b1331986ada7952ddbcff824364b3470
Alexander Uvizhev <uvizhe@yandex.ru>
2022-06-26 11:38:09 +0500

    store alias config in db

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,69 @@
 use argon2::{self, Config};
-use dialoguer::{Input, Password};
+use dialoguer::{Input, Select, Password};
+use home::home_dir;
+use pickledb::{PickleDb, PickleDbDumpPolicy};
+use serde::{Deserialize, Serialize};
 use sha2::{Sha224, Digest};
 
+const DB_FILE: &str = ".psh.db";
 // TODO: Add reduced character set password generation ability
-// TODO: Make global salt/password to add to local salt or password (so attacker couldn't easily
+// TODO: Make global salt/password to add to local salt or secret (so attacker couldn't easily
 //       guess inputs).
 // TODO: Show generated password until user hits Ctrl-C to exit program (then crear password from
 //       console)
+
+#[derive(Copy, Clone, Serialize, Deserialize)]
+enum CharSet {
+    Standard,
+    Reduced,
+}
+
 fn main() {
+    let mut db: PickleDb;
+
+    let mut db_file = home_dir().unwrap();
+    db_file.push(DB_FILE);
+
+    if db_file.exists() {
+        db = PickleDb::load_json(db_file, PickleDbDumpPolicy::AutoDump).unwrap();
+    } else {
+        db = PickleDb::new_json(db_file, PickleDbDumpPolicy::AutoDump);
+    }
+
     let alias: String = Input::new()
         .with_prompt("Alias")
         .interact_text()
         .unwrap();
 
-    let password = Password::new()
-        .with_prompt("Password")
+    let charset_config: CharSet;
+    if let Some(conf) = db.get(&alias) {
+        charset_config = conf;
+    } else {
+        println!("Looks like you use this alias for the first time.
+Please, select preferred character set for passwords that'll be generated
+for this alias."
+        );
+        println!("Note: Standard character set consists of all printable ASCII characters
+while Reduced set includes only letters and digits."
+        );
+        let sets = vec![CharSet::Standard, CharSet::Reduced];
+        let charset = Select::new()
+            .items(&vec!["Standard", "Reduced"])
+            .default(0)
+            .interact()
+            .unwrap();
+        db.set(&alias, &sets[charset]).unwrap();
+        charset_config = sets[charset];
+    }
+
+    let secret = Password::new()
+        .with_prompt("Secret")
         .interact()
         .unwrap();
 
     let salt = Sha224::digest(alias); // Make salt satisfy length criterium
     let config = Config::default();
-    let hash = argon2::hash_raw(password.as_ref(), &salt, &config).unwrap();
+    let hash = argon2::hash_raw(secret.as_ref(), &salt, &config).unwrap();
 
     let mut ascii_bytes = Vec::new();
     for byte in hash {
